e8e8dc83f0238ee5e210464414751f9f6a855d62
EhsanLaptop <ehsan.cse05@gmail.com>
2024-09-27 03:41:06 +0600

    initiate

---
+++ TemplateConfig/Controllers/DashboardController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TemplateConfig.Models;
+
+namespace TemplateConfig.Controllers
+{
+	public class DashboardController : Controller
+	{
+		private readonly EmpDataAccess _empDataAccess;
+
+		public DashboardController(IConfiguration configuration)
+		{
+			_empDataAccess = new EmpDataAccess(configuration);
+		}
+		public IActionResult Index()
+		{
+            List<Employee> emplst= _empDataAccess.GetAllEmployees();
+			int userCount= emplst.Count();
+            ViewBag.UserRegistrationsCount = userCount;
+            return View();
+		}
+
+		public IActionResult Create() 
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Employee employee)
+		{
+            _empDataAccess.AddEmployee(employee);
+            return View(employee);
+		}
+
+		public IActionResult EmpList()
+		{
+			var users = _empDataAccess.GetAllEmployees();
+			return View(users);
+		}
+
+        public IActionResult Edit(int id)
+        {
+            var employee = _empDataAccess.GetEmployeeById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee employee)
+        {
+
+                _empDataAccess.UpdateEmployee(employee);
+                return RedirectToAction("Index");        
+        }
+    }
+}
---
+++ TemplateConfig/Models/EmpDataAccess.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace TemplateConfig.Models
+{
+    public class EmpDataAccess
+    {
+        private readonly string _connectionString;
+
+        public EmpDataAccess(IConfiguration configaration)
+        {
+            _connectionString = configaration.GetConnectionString("DefaultConnection");
+        }
+        public void AddEmployee(Employee employee)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO employee (Name,Position,Salary) VALUES (@Name, @Position, @Salary)", con);
+                cmd.CommandType = CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Position", employee.Position);
+                cmd.Parameters.AddWithValue("@Salary", employee.Salary);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM employee", con);
+                cmd.CommandType = CommandType.Text;
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    employees.Add(new Employee
+                    {
+                        Id = Convert.ToInt32(rdr["Id"]),
+                        Name = rdr["Name"].ToString(),
+                        Position = rdr["Position"].ToString(),
+                        Salary = Convert.ToDecimal(rdr["Salary"])
+                    });
+                }
+                con.Close();
+            }
+
+            return employees;
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            Employee employee = null;
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM employee WHERE Id = @Id", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                if (rdr.Read())
+                {
+                    employee = new Employee
+                    {
+                        Id = Convert.ToInt32(rdr["Id"]),
+                        Name = rdr["Name"].ToString(),
+                        Position = rdr["Position"].ToString(),
+                        Salary = Convert.ToDecimal(rdr["Salary"])
+                    };
+                }
+                con.Close();
+            }
+
+            return employee;
+        }
+
+
+        public void UpdateEmployee(Employee employee)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE employee SET Name = @Name, Position = @Position, Salary = @Salary WHERE Id = @Id", con);
+                cmd.CommandType = CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Id", employee.Id);
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Position", employee.Position);
+                cmd.Parameters.AddWithValue("@Salary", employee.Salary);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
